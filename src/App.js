45eb6b235253146f09178ea7e8c3efb6c7b848bc
xbaysal11 <baisalbek.daniiarov@gmail.com>
2023-09-17 01:24:34 +0600

    FIX

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import Category from "./Category/Category";
+// import { Route, Routes, Navigate } from "react-router-dom";
+// import Category from "./Category/Category";
 // import Product from "./Product/Product";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      wetqetw
+      {/* <Routes>
         <Route path="/:lang" element={<Category />} />
-        {/* <Route path={"/:lang/category/:categoryId"} element={<Product />} /> */}
+        <Route path={"/:lang/category/:categoryId"} element={<Product />} />
         <Route index element={<Navigate to="/ru" replace />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
