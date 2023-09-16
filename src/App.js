6bdb04a4dc69e44b378b8a5b11b3c421c773b98c
xbaysal11 <baisalbek.daniiarov@gmail.com>
2023-09-16 17:00:55 +0600

    FIx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Category from "./Category/Category";
 import Product from "./Product/Product";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route index path="/:lang" element={<Category />} />
+        <Route path="/:lang" element={<Category />} />
         <Route path={"/:lang/category/:categoryId"} element={<Product />} />
+        <Route index element={<Navigate to="/ru" replace />} />
       </Routes>
     </div>
   );
