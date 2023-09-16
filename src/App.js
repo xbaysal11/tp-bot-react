6a967d93ff66d41fa9c332093620c5d5b81aa6cb
xbaysal11 <baisalbek.daniiarov@gmail.com>
2023-09-17 01:23:10 +0600

    FIX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Category from "./Category/Category";
-import Product from "./Product/Product";
+// import Product from "./Product/Product";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/:lang" element={<Category />} />
-        <Route path={"/:lang/category/:categoryId"} element={<Product />} />
+        {/* <Route path={"/:lang/category/:categoryId"} element={<Product />} /> */}
         <Route index element={<Navigate to="/ru" replace />} />
       </Routes>
     </div>
--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,84 +1,84 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import { ref, get, child } from "firebase/database";
+// import React, { useState, useEffect } from "react";
+// import { Link, useParams } from "react-router-dom";
+// import { ref, get, child } from "firebase/database";
 
-import db from "../firebase";
-import "./Product.css";
+// import db from "../firebase";
+// import "./Product.css";
 
-export default function Product() {
-  const { lang, categoryId } = useParams();
-  const [products, setProducts] = useState([]);
+// export default function Product() {
+//   const { lang, categoryId } = useParams();
+//   const [products, setProducts] = useState([]);
 
-  const getProducts = (lang, categoryId) => {
-    get(child(ref(db), `${lang}/products`))
-      .then((snapshot) => {
-        if (snapshot?.exists()) {
-          setProducts(
-            snapshot
-              ?.val()
-              ?.filter((i) => String(i?.categoryId) === String(categoryId))
-          );
-        } else {
-          console.log("no data");
-        }
-      })
-      .catch((error) => console.log(error));
-  };
-  const imageUrl = (url) => {
-    const link =
-      url?.split("/")?.[2] === "drive.google.com"
-        ? `https://drive.google.com/uc?export=view&id=${url?.split("/")?.[5]}`
-        : url;
-    return link;
-  };
+//   const getProducts = (lang, categoryId) => {
+//     get(child(ref(db), `${lang}/products`))
+//       .then((snapshot) => {
+//         if (snapshot?.exists()) {
+//           setProducts(
+//             snapshot
+//               ?.val()
+//               ?.filter((i) => String(i?.categoryId) === String(categoryId))
+//           );
+//         } else {
+//           console.log("no data");
+//         }
+//       })
+//       .catch((error) => console.log(error));
+//   };
+//   const imageUrl = (url) => {
+//     const link =
+//       url?.split("/")?.[2] === "drive.google.com"
+//         ? `https://drive.google.com/uc?export=view&id=${url?.split("/")?.[5]}`
+//         : url;
+//     return link;
+//   };
 
-  useEffect(() => {
-    console.log("product");
-    lang && categoryId && getProducts(lang, categoryId);
-  }, [lang, categoryId]);
+//   useEffect(() => {
+//     console.log("product");
+//     lang && categoryId && getProducts(lang, categoryId);
+//   }, [lang, categoryId]);
 
-  return (
-    <div className="products">
-      <Link to={`/${lang}`} className="back-link">
-        <button className="back-button">
-          {lang === "kg" ? "Артка" : "Назад"}
-        </button>
-      </Link>
-      <div className={products?.length > 0 ? "products-list" : ""}>
-        {products?.length > 0 ? (
-          products?.map((i) => (
-            <div key={i?.id} className="product">
-              <img src={imageUrl(i?.imageUrl)} alt="product" />
-              <div className="">
-                <span className="label-text text-ultra">{"Артикул: "}</span>
-                <span className="text-small">{i?.id}</span>
-              </div>
-              <div>
-                <span className="label-text text-ultra">
-                  {lang === "kg" ? "Аталышы: " : "Название: "}
-                </span>
-                <b>{i?.name}</b>
-              </div>
-              <div className="">
-                <span className="label-text text-ultra">
-                  {lang === "kg" ? "Өлкө: " : "Страна: "}
-                </span>
-                <span className="text-small">{i?.production}</span>
-              </div>
-              <div className="">
-                <span className="label-text text-ultra">
-                  {lang === "kg" ? "Аныктама: " : "Описание: "}
-                </span>
-                <span className="text-small">{i?.description}</span>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="text-center text-small">
-            {lang === "kg" ? "Тизме бош 🌞" : "Cписок пустой 🌞"}
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="products">
+//       <Link to={`/${lang}`} className="back-link">
+//         <button className="back-button">
+//           {lang === "kg" ? "Артка" : "Назад"}
+//         </button>
+//       </Link>
+//       <div className={products?.length > 0 ? "products-list" : ""}>
+//         {products?.length > 0 ? (
+//           products?.map((i) => (
+//             <div key={i?.id} className="product">
+//               <img src={imageUrl(i?.imageUrl)} alt="product" />
+//               <div className="">
+//                 <span className="label-text text-ultra">{"Артикул: "}</span>
+//                 <span className="text-small">{i?.id}</span>
+//               </div>
+//               <div>
+//                 <span className="label-text text-ultra">
+//                   {lang === "kg" ? "Аталышы: " : "Название: "}
+//                 </span>
+//                 <b>{i?.name}</b>
+//               </div>
+//               <div className="">
+//                 <span className="label-text text-ultra">
+//                   {lang === "kg" ? "Өлкө: " : "Страна: "}
+//                 </span>
+//                 <span className="text-small">{i?.production}</span>
+//               </div>
+//               <div className="">
+//                 <span className="label-text text-ultra">
+//                   {lang === "kg" ? "Аныктама: " : "Описание: "}
+//                 </span>
+//                 <span className="text-small">{i?.description}</span>
+//               </div>
+//             </div>
+//           ))
+//         ) : (
+//           <div className="text-center text-small">
+//             {lang === "kg" ? "Тизме бош 🌞" : "Cписок пустой 🌞"}
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
