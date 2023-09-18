57ca4f49217e0cd78318956427f01ac082c8b29d
xbaysal11 <baisalbek.daniiarov@gmail.com>
2023-09-18 11:38:26 +0600

    FIX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import Category from "./Category/Category";
-import Product from "./Product/Product";
+// import { Route, Routes, Navigate } from "react-router-dom";
+// import Category from "./Category/Category";
+// import Product from "./Product/Product";
 import { useEffect } from "react";
 
 const tg = window.Telegram.WebApp;
@@ -13,11 +13,12 @@ function App() {
   
   return (
     <>
-      <Routes>
+    twwewe
+      {/* <Routes>
         <Route path="/:lang" element={<Category />} />
         <Route path={"/:lang/category/:categoryId"} element={<Product />} />
         <Route index element={<Navigate to="/ru" replace />} />
-      </Routes>
+      </Routes> */}
     </>
   );
 }
--- src/Category/Category.jsx
+++ src/Category/Category.jsx
@@ -1,47 +1,47 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams, useLocation } from "react-router-dom";
-import { ref, get, child } from "firebase/database";
+// import React, { useEffect, useState } from "react";
+// import { Link, useParams, useLocation } from "react-router-dom";
+// import { ref, get, child } from "firebase/database";
 
-import db from "../firebase";
-import "./Category.css";
+// import db from "../firebase";
+// import "./Category.css";
 
-export default function Category() {
-  const { lang } = useParams();
-  const location = useLocation();
+// export default function Category() {
+//   const { lang } = useParams();
+//   const location = useLocation();
 
-  const [categories, setCategories] = useState([]);
+//   const [categories, setCategories] = useState([]);
 
-  const getCategories = (lang) => {
-    get(child(ref(db), `${lang}/categories`))
-      .then((snapshot) => {
-        if (snapshot?.exists()) {
-          setCategories(snapshot?.val());
-        } else {
-          console.log("no data");
-        }
-      })
-      .catch((error) => console.log(error));
-  };
+//   const getCategories = (lang) => {
+//     get(child(ref(db), `${lang}/categories`))
+//       .then((snapshot) => {
+//         if (snapshot?.exists()) {
+//           setCategories(snapshot?.val());
+//         } else {
+//           console.log("no data");
+//         }
+//       })
+//       .catch((error) => console.log(error));
+//   };
 
-  useEffect(() => {
-    console.log("category");
-    getCategories(lang);
-  }, [lang]);
+//   useEffect(() => {
+//     console.log("category");
+//     getCategories(lang);
+//   }, [lang]);
 
-  return (
-    <div className="categories">
-      <div className="categories-list">
-        {categories?.length > 0 &&
-          categories?.map((category) => (
-            <Link
-              to={`${location?.pathname}/category/${category?.id}`}
-              key={category?.id}
-              className="category"
-            >
-              <div>{category?.name}</div>
-            </Link>
-          ))}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="categories">
+//       <div className="categories-list">
+//         {categories?.length > 0 &&
+//           categories?.map((category) => (
+//             <Link
+//               to={`${location?.pathname}/category/${category?.id}`}
+//               key={category?.id}
+//               className="category"
+//             >
+//               <div>{category?.name}</div>
+//             </Link>
+//           ))}
+//       </div>
+//     </div>
+//   );
+// }
