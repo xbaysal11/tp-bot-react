6d186278cbd28135928cc1ffb55d10830ce57b38
xbaysal11 <baisalbek.daniiarov@gmail.com>
2023-09-18 12:40:28 +0600

    FIX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+
 import Category from "./Category/Category";
 import Product from "./Product/Product";
 import Language from "./Language/Language";
-import { useEffect } from "react";
 
 const tg = window.Telegram.WebApp;
 
@@ -12,14 +13,14 @@ function App() {
   });
 
   return (
-    <>
+    <div className="App">
       <Routes>
         <Route path="/" element={<Language />} />
         <Route path="/:lang" element={<Category />} />
         <Route path={"/:lang/category/:categoryId"} element={<Product />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
